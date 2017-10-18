15e5563d06e207ae9fdbd1e5d46d26f186cfb52c
Jan Karger <punkerat76@gmail.com>
2017-10-18 22:42:17 +0200

    Source from Blog with latest changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TextBoxInputMaskBehavior
+
 TextBoxInputMaskBehavior from Blindmeis's Blog
+
+https://blindmeis.wordpress.com/2015/01/20/wpf-textbox-input-behavior/
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace TextBoxInputMaskBehavior_Sample
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
